e00ee64077ab9e72e3ba675a110e71fac6ab3efd
Lee Gray <S00188181@mail.itsligo.ie>
2020-01-26 15:32:16 +0000

    abstract wont work

---
+++ q1/Band.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace q1
+{
+    public abstract class  Band
+    {
+        // props
+        public string BandName { get; set; }
+
+        public int YearFormed { get; set; }
+
+        public List<Genre> Genres { get; set; }
+
+
+        public  Band()
+        {
+
+        }
+
+        public  Band(string BName, int YFormed)
+        {
+            BandName = BName;
+            YearFormed = YFormed;
+        }
+
+        public class Rock : Band
+        {
+            public override string ToString()
+            {
+                return (this.BandName + " - Rock");
+            }
+        }
+
+        public class Pop : Band
+        {
+            public override string ToString()
+            {
+                return (this.BandName + " - Pop");
+            }
+        }
+
+        public class Indie : Band
+        {
+            public override string ToString()
+            {
+                return (this.BandName + " - Indie");
+            }
+        }
+
+        public class Genre
+        {
+            public string GenreName { get; set; }
+            
+        }
+
+        public override string ToString()
+        {
+            return $"{BandName}";
+        }
+    }
+}
--- q1/MainWindow.xaml.cs
+++ q1/MainWindow.xaml.cs
@@ -24,5 +24,34 @@ namespace q1
         {
             InitializeComponent();
         }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            Band b1 = new Band("jdskjfns", 1970);
+            Band b2 = new Rock("asdf", 1999);
+            Band b3 = new Band("help", 2000);
+            Band b4 = new Band("me", 2002);
+            Band b5 = new Band("the killers", 2005);
+            Band b6 = new Band("coldplay", 2002);
+
+            List<Band> allbands = new List<Band>();
+            allbands.Add(b1);
+            allbands.Add(b2);
+            allbands.Add(b3);
+            allbands.Add(b4);
+            allbands.Add(b5);
+            allbands.Add(b6);
+
+            Lbox.ItemsSource = allbands.OrderBy(b => b.BandName);
+
+            //setting contents of ComboBox
+            cbxGenre.ItemsSource = new string[] { "All", "Rock", "Indie", "Pop" };
+            
+
+        }
+
+        
     }
+
 }
+
